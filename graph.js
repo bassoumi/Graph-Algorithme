c34e7e8b2c8a319721020b56e913b665b5585c06
ILYESS BASSOUMI <156471600+bassoumi@users.noreply.github.com>
2025-05-02 20:00:32 +0100

    breadth and bapth

---
+++ graph.js
@@ -0,0 +1,39 @@
+const graph = {
+    a:['c', 'b'],
+    b:['d'],
+    c:['e'],
+    d:['f'],
+    e:[],
+    f:[]
+};
+
+const deapth = (graph,source)=>{
+    const stack = [source];
+
+    while(stack.length >0 ){
+        const curreent = stack.pop();
+        console.log(curreent);
+
+        for (let neighbor of graph[curreent]){
+            stack.push(neighbor);
+        }
+    }
+
+}
+
+const breadth = (graph ,source)=>{
+    const queue = [source]
+    while(queue.length >0){
+        const current = queue.shift();
+        console.log(current);
+
+        for (let neighbor of graph[current]){
+            queue.push(neighbor);
+        }
+ 
+    }
+}
+
+breadth(graph,'a');
+console.log('------------------------------------------')
+deapth(graph,'a')
